1a49ea23dd3a10d120fbe1a84acf87dd9ff7b5d4
Anuj Bansal <ANUJ@ramboll.com>
2022-01-01 16:40:45 +0530

    first commit

---
+++ server/src/loaders/userLoader.ts
@@ -0,0 +1,28 @@
+import { User } from ".prisma/client";
+import Dataloader from "dataloader";
+import { prisma } from "..";
+
+type BatchUser = (ids: number[]) => Promise<User[]>;
+
+const batchUsers: BatchUser = async (ids) => {
+  console.log(ids);
+  const users = await prisma.user.findMany({
+    where: {
+      id: {
+        in: ids,
+      },
+    },
+  });
+
+  const userMap: { [key: string]: User } = {};
+
+  users.forEach((user) => {
+    userMap[user.id] = user;
+  });
+  console.log("userMap",userMap);
+  console.log(ids.map((id) => userMap[id]));
+  return ids.map((id) => userMap[id]);
+};
+
+//@ts-ignore
+export const userLoader = new Dataloader<number, User>(batchUsers);
---
+++ server/src/resolvers/Mutation/Mutation.ts
@@ -0,0 +1,6 @@
+import { authResolvers } from "./auth";
+import { postResolvers } from "./post";
+export const Mutation = {
+  ...postResolvers,
+  ...authResolvers,
+};
